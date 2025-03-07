2d14fa55f547fc1d94556ef648c2fe8a6ab34d82
Astrobot <autobot268@gmail.com>
2025-03-07 19:46:39 +0530

    Vite config updated with correct asset Include path 02

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base:"./",
   plugins: [react()],
-  assetsInclude: ['src/**/*.jpg']
+  assetsInclude: ['src/resource/Compressed_Image/*.jpg']
 })
